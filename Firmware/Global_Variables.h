8bf2e1cbb8c8fd43acdfa67afa79c188890f1d8e
KingOfHate <kalvas.spam@gmail.com>
2026-02-14 21:32:06 +0100

    Add files via upload

---
+++ Firmware/Global_Variables.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <Arduino.h>
+
+extern const unsigned char image_battery_100_bits[] PROGMEM;
+extern const unsigned char image_music_play_bits[] PROGMEM;
+extern const unsigned char image_music_next_bits[] PROGMEM;
+extern const unsigned char image_music_previous_bits[] PROGMEM;
+extern const unsigned char image_music_record_bits[] PROGMEM;
+extern const unsigned char image_music_headphones_bits[] PROGMEM;
+extern const unsigned char image_wifi_full_bits[] PROGMEM;
+extern const unsigned char image_network_4_bars_bits[] PROGMEM;
+extern const unsigned char image_menu_bits[] PROGMEM;
+extern const unsigned char image_menu_options_bits[] PROGMEM;
+extern const unsigned char image_shuffle_2_svgrepo_com_bits[] PROGMEM;
+extern const unsigned char image_loop_svgrepo_com_bits[] PROGMEM;
+extern const unsigned char image_cards_hearts_bits[] PROGMEM;
+extern const unsigned char image_add_svgrepo_com_1_bits[] PROGMEM;
